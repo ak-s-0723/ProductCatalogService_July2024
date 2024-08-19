12b0837bc16236a4107e7d0171fa03af3ecacabc
khannaanurag1 <khannaanurag1@gmail.com>
2024-08-19 22:29:50 +0530

    FetchType and FatchMode Combination Demo

--- src/main/java/org/example/productcatalogservice_july2024/models/Category.java
+++ src/main/java/org/example/productcatalogservice_july2024/models/Category.java
@@ -5,6 +5,9 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.util.List;
 
@@ -16,6 +19,17 @@ public class Category extends BaseModel {
 
     private String description;
 
-    @OneToMany(mappedBy = "category",fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "category",fetch = FetchType.LAZY)
+    //@Fetch(FetchMode.SELECT)
+    //@BatchSize(size=10)
     private List<Product> products;
 }
+//N+1 Problem  => 1 + 6 ,
+//if(n == N) -> 1 + 1 n=6 -> 1 + 1
+//if(n < N) -> 1 + N/n , n = 3 ,  1+2
+//if(n>N) -> 1+1
+
+
+//Hypothesis
+//Fetch Mode JOIN has dominance over any fetch type
+//In case of FetchMode select and subselect, FetchType is getting obeyed/honored
--- src/test/java/org/example/productcatalogservice_july2024/repos/CategoryRepoTest.java
+++ src/test/java/org/example/productcatalogservice_july2024/repos/CategoryRepoTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -28,4 +30,32 @@ class CategoryRepoTest {
 
     }
 
+
+    @Test
+    @Transactional
+    public void demonstrateFetchModeFetchType() {
+        Category c = categoryRepo.findById(2L).get();
+        for(Product product : c.getProducts()) {
+            System.out.println(product.getDescription());
+        }
+    }
+
+    @Test
+    @Transactional
+    public void nPlusOneProblem() {
+        List<Category> categoryList = categoryRepo.findAll();
+        for(Category category : categoryList) {
+            if(!category.getProducts().isEmpty()) {
+                List<Product> products = category.getProducts();
+                for(Product product : products) {
+                   System.out.println(product.getDescription());
+               }
+            }
+        }
+    }
 }
+
+
+//1 category = parent ->  n children
+//
+//        select * from p where (select * from category .....)
